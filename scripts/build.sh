6a4fbd83bf06ee5ce833e745f7f201b6c0ca2b9e
Magnus Hallin <mhallin@fastmail.com>
2016-05-05 22:53:28 +0200

    Add build script to compile all environments

---
+++ scripts/build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -eu
+
+if [ ! -f package.json ]; then
+    echo 'This must be run from the source root of the project'
+    exit 1
+fi
+
+if [ ! -d node_modules ]; then
+    echo 'The NPM package dependencies must have been installed'
+    exit 1
+fi
+
+rm -rf ./dist
+
+for env in development production; do
+    echo "Compiling with environment '$env'"
+
+    NODE_ENV=$env ./node_modules/.bin/webpack --expose
+done
